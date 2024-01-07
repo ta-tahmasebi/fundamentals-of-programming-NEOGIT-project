6dfa867c93f4396e90fb59ff0c2e878ab5329ba9
Amirmahdi Tahmasebi <gm.ta.tahmasebi@gmail.com>
2024-01-07 19:07:20 +0330

    add 'change global name' and 'change global email' option

--- neogit.c
+++ neogit.c
@@ -2,12 +2,94 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
+
+//   *(IN THE NAME OF GOD)*
+//**  AMIRMAHDI TAHMASEBI   **
+
+
+char ** tokenizeInput(int* len, char** argv){
+    if(*len > 95){
+        printf("Too many arguments!\n");
+        exit(-1);
+    }
+    char** input = (char**)calloc(110, sizeof(char*));
+    int i;
+    for(i = 0; i < *len; i++)
+        *(input + i) = argv[i];
+    for(i; i< 100; i++)
+        *(input + i) = (char *)calloc(2,sizeof(char));
+    return input;
+}
+char equalStrings(char* a, char* b){
+    if(!strcmp(a,b))
+        return 1;
+    return 0;
+}
+
+char strcheck(char* string, char ch){ //salam , a -> 1 // salam , i -> 0
+    int i = 0;
+    while(string[i] != 0){
+        if(string[i] == ch){
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+void changeNameGlobal(char* name){
+    if(strlen(name) > 70){
+        printf("Your name is too long!\n");
+        exit(-1);
+    }
+    if(strcheck(name, '\"')){
+        printf("Your name is invalid!\n");
+        exit(-1);
+    }
+    FILE * f = fopen("d://SETTINGS//config//name", "w");
+    if(f == NULL){
+        printf("an unkown problem!\n");
+        exit(-1);
+    }
+    fprintf(f, name);
+    fclose(f);
+    printf("global name changed successfully\n");
+}
+
+void changeEmailGlobal(char* email){
+    if(strlen(email) > 150){
+        printf("Your email is too long!\n");
+        exit(-1);
+    }
+    if(strcheck(email, '\"')){
+        printf("Your email is invalid!\n");
+        exit(-1);
+    }
+    FILE * f = fopen("d://SETTINGS//config//email", "w");
+    if(f == NULL){
+        printf("an unkown problem!\n");
+        exit(-1);
+    }
+    fprintf(f, email);
+    fclose(f);
+    printf("global email changed successfully\n");
+}
+
 
 int main(int argc, char* argv[]){
-    printf("%d\n", argc);
-    for(int i = 0; i < argc; i++){
-        printf("|%s|  ", argv[i]);
-    }
-    //system("mkdir salam");
+    //get inputs        #############################
+    int len = argc;
+    char** input = tokenizeInput(&len, argv);
+    //end of getting    #############################
+
+    // neogim config -global user.name
+    if(equalStrings(input[1], "config") && equalStrings(input[2], "-global") && equalStrings(input[3], "user.name") && strcasecmp(input[4], "") && equalStrings(input[5], "") && len == 5)
+        changeNameGlobal(input[4]);
+    // neogim config -global user.email
+    if(equalStrings(input[1], "config") && equalStrings(input[2], "-global") && equalStrings(input[3], "user.email") && strcasecmp(input[4], "") && equalStrings(input[5], "") && len == 5)
+        changeEmailGlobal(input[4]);
+
+
     return 0;
 }
