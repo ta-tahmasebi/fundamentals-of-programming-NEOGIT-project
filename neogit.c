ec8872f79045807d0fd5fbed434e38862245c628
Amirmahdi Tahmasebi <gm.ta.tahmasebi@gmail.com>
2024-01-07 23:36:52 +0330

    working on the alias command 1

--- neogit.c
+++ neogit.c
@@ -42,7 +42,7 @@ char equalStrings(char* a, char* b){
         return 1;
     return 0;
 }
-
+//Helpful additinal functions:
 char strcheck(char* string, char ch){ //salam , a -> 1 // salam , i -> 0
     int i = 0;
     while(string[i] != 0){
@@ -53,7 +53,58 @@ char strcheck(char* string, char ch){ //salam , a -> 1 // salam , i -> 0
     }
     return 0;
 }
+int ___findLen (char* str){ //only for deleting spaces
+    int i = 0;
+    while(*(str + i) != ' ' && *(str + i) != '\t' && *(str + i) != '\0')
+        i++;
+    return i;
+}
 
+char* ___find (char* str, int* p){ //only for deleting spaces
+    if(*(str + *p) == '\0')
+        return NULL;
+    while(*(str + *p) == ' ' || *(str + *p) == '\t')
+        (*p) ++;
+    return (str + *p);
+}
+
+void ___print(char* str, int len, char** final){ //only for deleting spaces
+    if (!len) return;
+    int index = 0;
+    while(*(final + index))
+        index++;
+    char* a = (char*)calloc(300, sizeof(char));
+    for(int i = 0; i < len; i++)
+        a[i] = *(str + i);
+    a[len] = '\0';
+    *(final + index) = a;
+}
+//End of additional functions
+
+
+char** delete_spaces (char* string){
+    char ** finaladdress = (char**)calloc(300,sizeof(char*));
+    int p = 0;
+    while(1){
+        char* address = ___find(string, &p);
+        if(!address) break;
+        int len = ___findLen(address);
+        p+= len;
+        ___print(address, len, finaladdress);
+    }
+    /* how to use:
+    int index = 0;
+    while(*(finaladdress + index)){
+        printf("%s ", *(finaladdress + index));
+        index++;
+    }
+    */
+    return finaladdress;
+}
+
+
+
+//main functions
 void changeNameGlobal(char* name){
     if(strlen(name) > 70){
         printf("Your name is too long!\n");
@@ -169,6 +220,11 @@ void changeEmail(char* email){
     printf("local email changed successfully\n");
 }
 
+void alias(char* newName, char** command){
+
+}
+
+//End of main functions
 
 int main(int argc, char* argv[]){
     //get inputs        #############################
@@ -188,5 +244,14 @@ int main(int argc, char* argv[]){
     // neogit config user.email
     if(equalStrings(input[1], "config") && equalStrings(input[2], "user.email") && strcasecmp(input[3], "") && equalStrings(input[4], "") && len == 4)
         changeEmail(input[3]);
+    //neogit config -gelobal alias.name
+    if(equalStrings(input[1], "config") && equalStrings(input[2], "-gelobal") && (input[3][0] == 'a' && input[3][1] == 'l' && input[3][2] == 'i' && input[3][3] == 'a' && input[3][4] == 's' && input[3][5] == '.' && strlen(input[3]) > 6) && strcasecmp(input[4], "") && equalStrings(input[5], "") && len == 5){       
+        if(strlen(input[4]) > 250){
+            printf("Your command is too long!\n");
+            exit(-1);
+        }
+        alias(input[3]+6, delete_spaces(input[4]));
+    }
+    
     return 0;
 }
